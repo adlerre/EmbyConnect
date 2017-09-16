06b371fe225145a523d9be7492c5f79f994bbca9
René Adler <rene.adler@electric-force.net>
2017-09-16 12:18:33 +0200

    remove user and password parameter

--- EmbyConnect.js
+++ EmbyConnect.js
@@ -9,8 +9,6 @@ program.description("inits EmbyConnect with given options or use defaults");
 program.usage("[options]");
 program.option("--host [hostname]", "hostname to mount the hack", "trailers.apple.com");
 program.option("-e, --emby [server address]", "Emby server address");
-program.option("-u, --user [username]", "username for Emby login");
-program.option("-p, --pass [password]", "password for Emby login");
 program.option("--skipSSL", "skip https server start", false);
 program.parse(process.argv);
 
