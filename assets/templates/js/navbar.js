3d10fde5b3d091bd6b13f8ee480ff84c749399bd
René Adler <rene.adler@electric-force.net>
2017-09-25 19:40:32 +0200

    check http status

--- assets/templates/js/navbar.js
+++ assets/templates/js/navbar.js
@@ -10,7 +10,7 @@ function loadMenuPage(event) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         try {
-            if (req.readyState == 4) {
+            if (req.readyState === 4 && req.status === 200) {
                 doc = req.responseXML
                 if (event)
                     event.success(doc);
@@ -31,30 +31,35 @@ function reloadNavbar(event) {
     if (navbarCurrentItemId === "settings") {
         var req = new XMLHttpRequest();
         req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                var doc = req.responseXML;
+            try {
+                if (req.readyState === 4 && req.status === 200) {
+                    var doc = req.responseXML;
 
-                var navItems = document.rootElement.getElementsByTagName("navigationItem");
-                var rxNavItems = doc.rootElement.getElementsByTagName("navigationItem");
+                    var navItems = document.rootElement.getElementsByTagName("navigationItem");
+                    var rxNavItems = doc.rootElement.getElementsByTagName("navigationItem");
 
-                var changed = (navItems.length != rxNavItems.length);
-                var i = 0;
-                while (!changed && i < navItems.length) {
-                    changed = (navItems[i].getAttribute("id") != rxNavItems[i].getAttribute("id"));
-                    i++;
+                    var changed = (navItems.length != rxNavItems.length);
+                    var i = 0;
+                    while (!changed && i < navItems.length) {
+                        changed = (navItems[i].getAttribute("id") != rxNavItems[i].getAttribute("id"));
+                        i++;
+                    }
+
+                    if (!changed) {
+                        logger.debug("reloadNavbar done: settings unchanged - no action");
+                        event.cancel();
+                    } else {
+                        var navbar = doc.rootElement.getElementByTagName("navigation");
+                        var currentIndex = (rxNavItems.length - 1).toString();
+                        navbar.setAttribute("currentIndex", currentIndex);
+
+                        logger.debug("reloadNavbar done: settings changed - reload");
+                        atv.loadAndSwapXML(doc);
+                    }
                 }
-
-                if (!changed) {
-                    logger.debug("reloadNavbar done: settings unchanged - no action");
-                    event.cancel();
-                } else {
-                    var navbar = doc.rootElement.getElementByTagName("navigation");
-                    var currentIndex = (rxNavItems.length - 1).toString();
-                    navbar.setAttribute("currentIndex", currentIndex);
-
-                    logger.debug("reloadNavbar done: settings changed - reload");
-                    atv.loadAndSwapXML(doc);
-                }
+            } catch (e) {
+                logger.error("caught exception while while reload navbar: " + e);
+                req.abort();
             }
         };
 
