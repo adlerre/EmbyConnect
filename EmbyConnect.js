9c1cd7254f28c843b15f9642dc39ef42d234b1b9
René Adler <rene.adler@electric-force.net>
2017-12-19 13:53:33 +0100

    add pidfile option

--- EmbyConnect.js
+++ EmbyConnect.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+var fs = require("fs");
 var program = require("commander");
 var Application = require(__dirname + "/lib/application");
 
@@ -10,6 +11,14 @@ program.usage("[options]");
 program.option("--host [hostname]", "hostname to mount the hack", "trailers.apple.com");
 program.option("-e, --emby [server address]", "Emby server address");
 program.option("--skipSSL", "skip https server start", false);
+program.option("--pidfile [pidfile]", "full pid file path");
 program.parse(process.argv);
 
+if (program.pidfile !== undefined) {
+    fs.writeFile(program.pidfile, process.pid, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 app.run(program);
